ed340aca4c7376f6309d60355769c956822943f4
Matanaiz <19516454860@163.com>
2026-09-11 14:14:00 +0800

    修复GameViewFx缺少StackPane导入的编译错误

--- src/main/java/com/bilitro/view/GameViewFx.java
+++ src/main/java/com/bilitro/view/GameViewFx.java
@@ -15,6 +15,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 import java.util.List;
